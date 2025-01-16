c700932040949b6324c333b689f7a4b99ea51216
Andy Stuckey <andystuckey@mail.com>
2025-01-16 12:55:15 -0600

    linked opentrivia api to the script

--- game.js
+++ game.js
@@ -11,12 +11,27 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json").then((res) => {
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple").then((res) => {
     return res.json();
     })
     .then((loadedQuestions) => {
-    questions = loadedQuestions;
-    startGame();
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map( loadedQuestion => {
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random()*3) + 1;
+        answerChoices.splice(formattedQuestion -1, 0, loadedQuestion.correct_answer);
+
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index+1)] = choice;
+        });
+
+        return formattedQuestion;
+    });
+        startGame();
     })
     .catch(err => {
         console.error(err);
