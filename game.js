e590ea11e0ba3cbb034d0b46e4f6f02d3af44d8a
Andy Stuckey <andystuckey@mail.com>
2025-01-20 23:52:54 -0600

    loading spinner

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -31,6 +33,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
         return formattedQuestion;
     });
+
         startGame();
     })
     .catch(err => {
@@ -48,6 +51,8 @@ startGame = () => {
     availableQuestions = [...questions];
     
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
